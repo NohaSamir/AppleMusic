55d6bff1b3e1b56db8442d502def1101e9c07aa7
nohasamir <eng.nohasamirsaad@gmail.com>
2024-06-28 13:58:28 +0300

    Project Setup

---
+++ app/src/main/java/com/vama/applemusic/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.vama.applemusic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightPrimary = Color(0xFF0473ec)
+val LightSecondary = Color(0xFF0d64d9)
+val LightTertiary = Color(0xFFE85184)
+
+val DarkPrimary = Color(0xFF0151b5)
+val DarkSecondary = Color(0xFF0a53a4)
+val DarkTertiary = Color(0xFFb23a67)
