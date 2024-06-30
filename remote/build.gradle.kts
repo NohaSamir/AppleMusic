b3f4f20cfb73aa474dffd2461c59a808da1531c0
nohasamir <eng.nohasamirsaad@gmail.com>
2024-06-30 21:26:05 +0300

    fix: remote layer * Add missing field feed to AlbumsFeedResponse.kt * Add hilt compiler

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt.plugin)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -38,5 +40,6 @@ android {
 
 dependencies {
     implementation(libs.bundles.remote.dependencies)
+    ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
 }
--- remote/src/main/java/com/vama/remote/model/AlbumsFeedResponse.kt
+++ remote/src/main/java/com/vama/remote/model/AlbumsFeedResponse.kt
@@ -1,6 +1,10 @@
 package com.vama.remote.model
 
 data class AlbumsFeedResponse(
+    val feed: AlbumsFeed
+) : RemoteModel
+
+data class AlbumsFeed(
     val results: List<AlbumRemoteModel>,
     val copyright: String
 ) : RemoteModel
